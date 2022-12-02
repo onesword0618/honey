6673e99f696cb938b1352476842b468a9ac760e8
onesword0618 <ao.akua.leo@gmail.com>
2022-12-03 01:18:59 +0900

    Fix: deploy tool

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ echo "This script is used to set the Typora theme."
 # Main Process
 function deploy() {
     cp -i ./honey.css ~/.config/Typora/themes/honey.css
-    cp -i ./honey ~/.config/Typora/themes/honey
+    cp -ri ./honey ~/.config/Typora/themes/
 }
 
 # Start
